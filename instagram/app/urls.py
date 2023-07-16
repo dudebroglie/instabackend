aa332225590971ce867ea7b2b2ddce7b5a3d216d
dudebroglie <guptagorang166@gmail.com>
2023-07-16 19:48:55 +0530

    first commit

---
+++ instagram/app/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path
+from app.view.user import CreateUser,LoginUserView,RetrieveUser,UpdateUser,DestroyUser
+from app.view.post import RetrieveUserPosts,CreatePost,RetrievePost,UpdatePost,DestroyPost,LikePost,CommentPost,FollowUser
+
+urlpatterns = [
+    path('user/create/',CreateUser.as_view()),
+    path('user/login/',LoginUserView.as_view()),
+    path('user/<int:pk>/',RetrieveUser.as_view()),
+    path('user/update/',UpdateUser.as_view()),
+    path('user/delete/<int:pk>/',DestroyUser.as_view()),
+
+    path('posts/',RetrieveUserPosts.as_view(),),
+    path('post/create/',CreatePost.as_view(),),
+    path('post/<int:pk>/',RetrievePost.as_view()),
+    path('post/update/<int:pk>/',UpdatePost.as_view()),
+    path('post/delete/<int:pk>/',DestroyPost.as_view()),
+    
+    path('post/like/<int:pk>/',LikePost.as_view()),
+
+    path('post/comment/<int:pk>/', CommentPost.as_view()),
+    path('post/comments/<int:pk>/', CommentPost.as_view()),
+
+    path('user/follow/<int:pk>/', FollowUser.as_view()),
+]
